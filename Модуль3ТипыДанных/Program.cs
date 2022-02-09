05f1792a66f29d692d0d45196bc8b5949b05e7d9
Nikolai7832 <nikolai7832@yandex.ru>
2022-02-09 23:54:58 +0300

    Добавьте файлы проекта.

---
+++ Модуль3ТипыДанных/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Модуль3ТипыДанных
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Nikolai";
+            byte Age = 23;
+            bool Pet = false;
+            double ShoeSize = 39.5;
+            Console.WriteLine("My name is " + Name);
+            Console.WriteLine("My age " + Age);
+            Console.WriteLine("Do i have a pet? " + Pet);
+            Console.WriteLine("My shoe size is " + ShoeSize);
+        }
+    }
+}
